88df3324264d74c525027782f3e36c13214d7e7a
Jim Yoon <jimyoon90@gmail.com>
2018-12-02 16:50:32 -0800

    working log in page, moved async funcs to actions instead of reducers, will be working showing all restaurants

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,12 +1,40 @@
 // import actionType constants
 import * as types from '../constants/actionTypes';
 
-export const logEmail = () => ({
+export const logEmail = (text) => ({
   type: types.LOG_EMAIL,
-  // payload: 
+  payload: text,
 });
 
-export const logIn = () => ({
-  type: types.LOG_IN,
-  // payload: sides,
+export const logPass = (text) => ({
+  type: types.LOG_PASSWORD,
+  payload: text,
 });
+
+// export const logIn = () => ({
+//   type: types.LOG_IN,
+// });
+
+export const logIn = (state) => {
+  return (dispatch) => {
+    fetch('http://192.168.86.234:3000/user/login', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        email: state.emailField,
+        password: state.passwordField,
+      }),
+    })
+      .then(res => res.json())
+      .then((user) => {
+        dispatch({
+          type: types.LOG_IN,
+          payload: user,
+        });
+      });
+  };
+};
