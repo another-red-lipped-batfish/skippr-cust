8a06bf3730e8823646da079a715e51f0e2219f17
Darryl Amour <darryl.amour@gmail.com>
2018-12-05 09:05:44 -0800

    linter compliance.

--- client/reducers/menusReducer.js
+++ client/reducers/menusReducer.js
@@ -4,19 +4,16 @@ const initialState = {
   menu: [],
 };
 
-const menusReducer = (state=initialState, action) => {
+const menusReducer = (state = initialState, action) => {
   let menu;
-  switch(action.type) {
-
+  switch (action.type) {
     case types.GET_MENU:
-      // console.log('payload is ', action.payload[0]);
       menu = action.payload;
-      // console.log('list is ', list)
       return {
         ...state,
         menu,
       };
-      
+
     default:
       return state;
   }
