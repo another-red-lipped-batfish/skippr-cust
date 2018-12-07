1479926025f4941195d138c03b1a14e28a7360b3
Darryl Amour <darryl.amour@gmail.com>
2018-12-07 11:09:35 -0800

    added action LOG_OUT so the application state is wiped upon log out of a user. adjusted fetch for verifying user to handle non 200 range responses

--- client/reducers/usersReducer.js
+++ client/reducers/usersReducer.js
@@ -8,22 +8,22 @@ const initialState = {
   phone: null,
   emailField: '',
   passwordField: '',
-  logged: false
+  loginErrorMsg: '',
+  logged: false,
 };
 
-const userReducer = (state=initialState, action) => {
+const userReducer = (state = initialState, action) => {
+  let id = null;
+  let firstName = null;
+  let lastName = null;
+  let email = null;
+  let phone = null;
+  let emailField = '';
+  let passwordField = '';
+  let loginErrorMsg = '';
+  let logged = false;
 
-  let id;
-  let firstName;
-  let lastName;
-  let email;
-  let phone;
-  let emailField;
-  let passwordField;
-  let logged;
-
-  switch(action.type) {
-
+  switch (action.type) {
     case types.LOG_EMAIL:
       emailField = action.payload;
       return {
@@ -33,23 +33,51 @@ const userReducer = (state=initialState, action) => {
 
     case types.LOG_PASSWORD:
       passwordField = action.payload;
-
       return {
         ...state,
         passwordField,
       };
-    
-    case types.LOG_IN:
-      const { user_id, user_firstname, user_lastname, user_email, user_phone} = action.payload;
 
-      id = user_id;
-      firstName = user_firstname;
-      lastName = user_lastname;
-      email = user_email;
-      phone = user_phone;
-      emailField = '';
-      passwordField = '';
-      logged = true;
+    case types.LOG_OUT:
+      return {
+        ...state,
+        id,
+        firstName,
+        lastName,
+        email,
+        phone,
+        emailField,
+        passwordField,
+        loginErrorMsg,
+        logged,
+      };
+
+    case types.LOG_IN:
+      // Determine if Login was successful
+      if (action.payload.statusCode >= 200 && action.payload.statusCode <= 299) {
+        // Success has status in the range 200-299
+        const { 
+          user_id, user_firstname, user_lastname,
+          user_email, user_phone
+        } = action.payload.user;
+
+        logged = true;
+        loginErrorMsg = '';
+        emailField = '';
+        passwordField = '';
+        id = user_id;
+        firstName = user_firstname;
+        lastName = user_lastname;
+        email = user_email;
+        phone = user_phone;
+      } else if (action.payload.statusCode >= 400 && action.payload.statusCode <= 499) {
+        logged = false;
+        loginErrorMsg = `Failed to Login - (Code: ${action.payload.statusCode})`;
+      } else {
+        logged = false;
+        loginErrorMsg = `Failed to Login: ${action.payload.errorText || ''} (Code: ${action.payload.statusCode})`;
+      }
+
       return {
         ...state,
         id,
@@ -60,6 +88,7 @@ const userReducer = (state=initialState, action) => {
         emailField,
         passwordField,
         logged,
+        loginErrorMsg,
       };
 
     default:
